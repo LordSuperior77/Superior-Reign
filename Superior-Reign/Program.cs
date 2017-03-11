98389ec5c9e2953c27e12b3b6eebdb04257ffd22
createjump <joeypence88@gmail.com>
2017-03-10 22:52:41 -0500

    OpenTK graphics

--- Superior-Reign/Program.cs
+++ Superior-Reign/Program.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
 
 namespace Superior_Reign
 {
-    class Program
+    class Program 
     {
         static void Main(string[] args)
         {
             Console.WriteLine("ReignEngine v0 startup");
+            
+            using (Game game = new Game())
+            { 
+                game.Run(60.0);
+
+            }
         }
     }
 }
